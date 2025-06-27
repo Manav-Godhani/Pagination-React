308e9e1eb762f7a18b26880a9ad6baf397bedc04
mnv-godhani <godhanik87@gmail.com>
2025-06-27 11:11:31 +0530

    Added Logic for dynamic page based on prd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-
   const [products,setProducts] = useState([]);
+  const [page,setPage] = useState(1);
 
   const fetchData = async () => {
     const res = await fetch('https://dummyjson.com/products');
@@ -23,13 +23,25 @@ function App() {
   return (
     <div>
       {
-        products.length>0 && <div className='products'>
-          {products.map((prd)=>{
+        products.length>0 && (<div className='products'>
+          {products.slice(page*10-10,page * 10).map((prd)=>{
             return <span className='products__single' key={prd.id}>
               <img src={prd.thumbnail} alt={prd.title}/>
               <span>{prd.title}</span>
             </span>
           })}
+        </div>
+      )}
+
+      {
+        products.length>0 && <div className='pagination'>
+          <span>⬅️</span>
+          {
+            [...Array(products.length/10)].map((_,i)=>{
+              return <span>{i + 1}</span>
+            })
+          }
+          <span>➡️</span>
         </div>
       }
     </div>
