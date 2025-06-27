44dfce66467f57e376748c9e309c56fdd0b0b554
mnv-godhani <godhanik87@gmail.com>
2025-06-27 10:49:16 +0530

    Product title displayed success

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [products,setProducts] = useState([]);
+
   const fetchData = async () => {
     const res = await fetch('https://dummyjson.com/products');
     const data = await res.json();
-    
+
+    if(data && data.products){
+      setProducts(data.products);
+    }
   }
 
   useEffect(()=>{
     fetchData();
   },[]);
 
-
+  console.log(products);
+  
   return (
-    <div className="App">
-      
+    <div>
+      {
+        products.length>0 && <div className='products'>
+          {products.map((prd)=>{
+            return <span>{prd.title}</span>
+          })}
+        </div>
+      }
     </div>
   );
 }
